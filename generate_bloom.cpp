8c7bc3bbbb4a255ceea478a4c74a5819a65291d0
AlexCurl <144244712+AlexanderKud@users.noreply.github.com>
2025-05-05 08:00:44 +0300

    Update generate_bloom.cpp

--- generate_bloom.cpp
+++ generate_bloom.cpp
@@ -150,7 +150,7 @@ auto main() -> int {
     auto bloom_create1 = [&]() {
         string bloomfile = "bloom1.bf";
         uint64_t n_elements = uint64_t(pow(2, block_width) * 1.5);
-        double error = 0.00000001;
+        double error = 0.000000001;
         Point P(puzzle_point);
         filter bf(n_elements, error);
         print_time(); cout << "Creating BloomFile1" << '\n';
@@ -171,7 +171,7 @@ auto main() -> int {
     auto bloom_create2 = [&]() {
         string bloomfile = "bloom2.bf";
         uint64_t n_elements = uint64_t(pow(2, block_width) * 1.5);
-        double error = 0.00000001;
+        double error = 0.000000001;
         Point P(puzzle_point_05);
         filter bf(n_elements, error);
         print_time(); cout << "Creating BloomFile2" << '\n';
