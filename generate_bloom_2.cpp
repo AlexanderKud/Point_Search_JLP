e426c77a4a4e07c25a1b7620f422395b5e7bf879
AlexCurl <144244712+AlexanderKud@users.noreply.github.com>
2025-05-12 05:39:58 +0300

    Update generate_bloom_2.cpp

--- generate_bloom_2.cpp
+++ generate_bloom_2.cpp
@@ -149,7 +149,7 @@ auto main() -> int {
     using filter = boost::bloom::filter<std::string, 32>;
     uint64_t n_elements = uint64_t(pow(2, block_width) * 1.0);
     double error = 0.0000000001;
-    int n_cores = 4;
+    int n_cores = 4; //actual number of processing cores but equal to some power of two value(2,4,8,16,32,64,...)
     uint64_t count = uint64_t(pow(2, block_width) / n_cores);
     Int add_key; add_key.SetInt64(count);
     Point Add_Point = secp256k1->ComputePublicKey(&add_key);
